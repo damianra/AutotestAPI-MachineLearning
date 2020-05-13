3057a752a8e293f78b4621b0f1cf417a8abfe739
damianra <44167699+damianra@users.noreply.github.com>
2020-05-13 13:47:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pickle
 import pandas as pd
 from flask import Flask, jsonify, request, render_template
+from flask_restful import Api
+from api import ApiTest
 import sklearn
 
 modelName = 'AutotestModel.sav'
@@ -8,6 +10,9 @@ modelName = 'AutotestModel.sav'
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'snvu124qchmf9483rcrc2er15q3f1ado13403'
 app.config['JSON_SORT_KEYS'] = False
+api = Api(app, prefix="/api/")
+
+api.add_resource(ApiTest, '/test-covid')
 
 
 def validacionAutotest(consulta):
@@ -66,33 +71,22 @@ def autotest():
     lista = validacionAutotest(consulta)
     id = int(df.tail(1)['id'] + 1)
     pred = clf.predict([lista])
-    data = { 'id': id,
-             'Temperatura': lista[0],
-             'Zona de riesgo': lista[1],
-             'Contacto con Algun enfermo': lista[2],
-             'Cansancio': lista[3],
-             'Perd. Olfato': lista[4],
-             'Perd. Gusto': lista[5],
-             'Tos o Dolor garganta': lista[6],
-             'Dificultad Resp.': lista[7],
-             'Situacion de Riesgo': lista[8],
-             'Clasificacion': pred[0]
-             }
+    dic = {'id': id,
+           'Temperatura': lista[0],
+           'Zona de riesgo': lista[1],
+           'Contacto con Algun enfermo': lista[2],
+           'Cansancio': lista[3],
+           'Perd. Olfato': lista[4],
+           'Perd. Gusto': lista[5],
+           'Tos o Dolor garganta': lista[6],
+           'Dificultad Resp.': lista[7],
+           'Situacion de Riesgo': lista[8],
+           'Clasificacion': pred[0]
+           }
     # df2 = pd.DataFrame(data)
-    df = df.append([data])
+    df = df.append([dic])
     df.to_csv('autotest.csv', mode='w', index=False)
-    return jsonify({
-        'Temperatura': lista[0],
-        'Zona de riesgo': lista[1],
-        'Contacto con Algun enfermo': lista[2],
-        'Cansancio': lista[3],
-        'Perd. Olfato': lista[4],
-        'Perd. Gusto': lista[5],
-        'Tos o Dolor garganta': lista[6],
-        'Dificultad Resp.': lista[7],
-        'Situacion de Riesgo': lista[8],
-        'Clasificacion': pred[0]
-    })
+    return jsonify({'data': dic, 'disclaimer': ''})
 
 
 @app.route('/data')
@@ -105,7 +99,6 @@ def data():
 @app.route('/')
 @app.route('/index')
 def index():
-
     return render_template('index.html')
 
 
@@ -123,27 +116,26 @@ def test():
     clf = pickle.load(open(modelName, 'rb'))
     df = pd.read_csv('autotest.csv')
     id = int(df.tail(1)['id'] + 1)
-    pred = clf.predict([[float(temp),int(zona),int(contacto),int(cansancio),int(perOlf), int(perGus), int(tos), int(difRes), int(pacRiesgo)]])
+    pred = clf.predict([[float(temp), int(zona), int(contacto), int(cansancio), int(perOlf), int(perGus), int(tos),
+                         int(difRes), int(pacRiesgo)]])
     dic = {
-            'id': id,
-            'Temperatura': float(temp),
-            'Zona de riesgo': int(zona),
-            'Contacto con Algun enfermo': int(contacto),
-            'Cansancio': int(cansancio),
-            'Perd. Olfato': int(perOlf),
-            'Perd. Gusto': int(perGus),
-            'Tos o Dolor garganta': int(tos),
-            'Dificultad Resp.': int(difRes),
-            'Situacion de Riesgo': int(pacRiesgo),
-            'Clasificacion': pred[0]
-        }
+        'id': id,
+        'Temperatura': float(temp),
+        'Zona de riesgo': int(zona),
+        'Contacto con Algun enfermo': int(contacto),
+        'Cansancio': int(cansancio),
+        'Perd. Olfato': int(perOlf),
+        'Perd. Gusto': int(perGus),
+        'Tos o Dolor garganta': int(tos),
+        'Dificultad Resp.': int(difRes),
+        'Situacion de Riesgo': int(pacRiesgo),
+        'Clasificacion': pred[0]
+    }
     df = df.append([dic])
-    print(dic)
     df.to_csv('autotest.csv', mode='w', index=False)
 
     return render_template('index.html', dic=dic)
 
 
 if __name__ == '__main__':
-
     app.run(debug=False)
